a55f3a094712d2e643d3be3f789472b96915bdaa
Tim <117385556+timpel301@users.noreply.github.com>
2022-11-22 10:27:35 +0100

    added second 'random' function

--- bash_script_tasks.sh
+++ bash_script_tasks.sh
@@ -4,5 +4,7 @@ echo This script is located in $location
 random=$RANDOM
 echo Here\'s a random number for you
 echo $random
+echo Here is an alternative where a defined range is given and shuffled followed by output of one line:
+shuf -i 1-100000 -n 1
 read -p "Text input: " Text
 echo $Text
